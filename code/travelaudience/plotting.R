eadbf380eabbbb2cea0dfc313cea61b76f998d36
Paul Teehan <paul.teehan@gmail.com>
2016-08-20 16:56:30 +0200

    more visualizations

--- code/travelaudience/plotting.R
+++ code/travelaudience/plotting.R
@@ -1,5 +1,5 @@
 
-
+source("convertRoute.R")
 plotAllRoutes <- function(code="LHR") {
   segments <- summarizeSegments()
   segments <- segments[to==code]
@@ -10,14 +10,41 @@ plotSegments <- function(segments, ...) {
   airport_data <- data.table(read.csv("/host/files/airports.csv", header = FALSE))[, list(code=V5, name=V3, country=V4, lat=V7, lng=V8)]
   g <- graph.data.frame(segments)
   library(intergraph)
+  library(network)
   n<-asNetwork(g)
   setkey(airport_data, code)
   n %v% "lat" <- airport_data[network.vertex.names(n)]$lat
   n %v% "lon" <- airport_data[network.vertex.names(n)]$lng
+  latmin <-  min(n %v% "lat", na.rm=TRUE) - 10
+  latmax <- max(n %v% "lat", na.rm=TRUE) + 10
+  lonmin <- min(n %v% "lon", na.rm=TRUE) - 10
+  lonmax <- max(n %v% "lon", na.rm=TRUE) + 10
   library(maps)
+  library(ggmap)
+  library(GGally)
   world <- fortify(map("world", plot = FALSE, fill = TRUE))
   world <- ggplot(world, aes(x = long, y = lat)) +
     geom_polygon(aes(group = group), color = "grey65",
-                 fill = "#f9f9f9", size = 0.2)
+                 fill = "#f9f9f9", size = 0.2) +
+    coord_quickmap(xlim = c(lonmin, lonmax),ylim = c(latmin, latmax))
   ggnetworkmap(world, n, size=1, great.circles=TRUE, segment.color="steelblue", ...) 
+}
+
+
+plotAirports <- function(codes=c("FRA", "LHR")) {
+  if(is.data.table(codes)) codes <- codes$to
+  airport_data <- data.table(read.csv("/host/files/airports.csv", header = FALSE))[, list(code=V5, name=V3, country=V4, lat=V7, lng=V8)]
+  setkey(airport_data, code)
+  airports <- airport_data[as.character(codes)]
+  latmin <-  min(airports$lat)- 10
+  latmax <- max(airports$lat) + 10
+  lonmin <- min(airports$lng) - 10
+  lonmax <- max(airports$lng) + 10
+  world <- fortify(map("world", plot = FALSE, fill = TRUE))
+  world <- ggplot(world, aes(x = long, y = lat)) + 
+                   geom_polygon(aes(group = group), color = "grey65",
+                 fill = "#f9f9f9", size = 0.2) +
+    coord_quickmap(xlim = c(lonmin, lonmax),ylim = c(latmin, latmax))
+  world + geom_point(data=airports, aes(x=lng, y=lat)) + 
+    geom_text(data=airports, aes(label=code, x=lng, y=lat), nudge_y=2, size=3)
 }
